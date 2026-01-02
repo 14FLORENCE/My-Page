a0d2e0577db6c7b5355fa2d3076930e9a386041e
SWAY <www.florencesway@gmail.com>
2026-01-02 17:53:26 +0300

    new form

--- js/main.js
+++ js/main.js
@@ -65,4 +65,80 @@ Menu
 
 })();
 
+/******************************************************************************************************************************
+Contact Form Enhancement
+*******************************************************************************************************************************/
+$(document).ready(function() {
+    const contactForm = $('#contactForm');
+    const submitBtn = $('#submitBtn');
+    const formMessage = $('#formMessage');
+
+    contactForm.on('submit', function(e) {
+        e.preventDefault();
+
+        // Show loading state
+        submitBtn.addClass('loading').prop('disabled', true);
+        submitBtn.html('<i class="fa fa-spinner fa-spin"></i> Sending...');
+
+        // Hide any previous messages
+        formMessage.hide();
+
+        // Submit the form
+        const formData = new FormData(this);
+
+        fetch(this.action, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.ok) {
+                // Success
+                formMessage.removeClass('error').addClass('success')
+                    .html('<i class="fa fa-check-circle"></i> Thank you! Your message has been sent successfully.')
+                    .fadeIn();
+                contactForm[0].reset();
+
+                // Scroll to message
+                $('html, body').animate({
+                    scrollTop: formMessage.offset().top - 100
+                }, 500);
+            } else {
+                throw new Error('Form submission failed');
+            }
+        })
+        .catch(error => {
+            // Error
+            formMessage.removeClass('success').addClass('error')
+                .html('<i class="fa fa-exclamation-triangle"></i> Oops! There was a problem sending your message. Please try again.')
+                .fadeIn();
+        })
+        .finally(() => {
+            // Reset button state
+            submitBtn.removeClass('loading').prop('disabled', false);
+            submitBtn.html('<i class="fa fa-paper-plane"></i> Send Message');
+        });
+    });
+
+    // Enhanced form validation feedback
+    contactForm.find('input, textarea').on('blur', function() {
+        const $field = $(this);
+        const $group = $field.closest('.form-group');
+
+        if ($field.val().trim() !== '') {
+            $group.addClass('has-value');
+        } else {
+            $group.removeClass('has-value');
+        }
+    });
+
+    // Auto-resize textarea
+    $('#contact-message').on('input', function() {
+        this.style.height = 'auto';
+        this.style.height = (this.scrollHeight) + 'px';
+    });
+});
+
 
